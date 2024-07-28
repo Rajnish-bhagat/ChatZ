f1adc7f8bf577480bc5b16bff295fc759a899a18
Rajnish Bhagat <rajnishbhagat4@gmail.com>
2024-07-28 20:02:15 +0530

    Contacts section completed

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -1,27 +1,51 @@
+import { useEffect } from "react";
 import NewDM from "./components/new-dm";
 import ProfileInfo from "./components/profile-info";
+import { apiClient } from "@/lib/api-client";
+import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
+import { useAppStore } from "@/store";
+import ContactList from "@/components/contact-list";
 
 const ContactsContainer = ()=>{
-    return (
-    <div className="relative md:w-[35vw] lg:w-[30vw] xl-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full ">
-      <div className="pt-3">
-          <Logo/>
-      </div>
-      <div className="my-5">
-          <div className="flex items-center justify-between pr-10">
-              <Title title="Direct Messages"/>
-              <NewDM/>
-          </div>
-      </div>
+  const {setDirectMessagesContacts, directMessagesContacts} = useAppStore();
+  useEffect(()=>{
+    const getContacts = async()=>{
+      const response = await apiClient.get(GET_DM_CONTACTS_ROUTES, {
+        withCredentials:true
+      });
 
-      <div className="my-5">
-          <div className="flex items-center justify-between pr-10">
-              <Title title="Channels"/>
-          </div>
-      </div>
-      <ProfileInfo/>
+      if(response.data.contacts){
+        setDirectMessagesContacts(response.data.contacts);
+
+      }
+
+    }
+    getContacts();
+  },[])
+
+  return (
+  <div className="relative md:w-[35vw] lg:w-[30vw] xl-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full ">
+    <div className="pt-3">
+        <Logo/>
     </div>
-  )
+    <div className="my-5">
+        <div className="flex items-center justify-between pr-10">
+            <Title title="Direct Messages"/>
+            <NewDM/>
+        </div>
+        <div className="max-h-[38vh] overflow-y-auto scrollbar-hidden">
+          <ContactList contacts={directMessagesContacts}/>
+        </div>
+    </div>
+
+    <div className="my-5">
+        <div className="flex items-center justify-between pr-10">
+            <Title title="Channels"/>
+        </div>
+    </div>
+    <ProfileInfo/>
+  </div>
+)
 } 
 
 
